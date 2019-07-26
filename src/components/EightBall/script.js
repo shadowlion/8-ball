99a614ee1e10324601b1bb3597fb1acd30721889
Justin Chiou <chiou.kai@gmail.com>
2019-07-26 13:28:14 -0400

    ping slack when someone fills out a question

--- src/components/EightBall/script.js
+++ src/components/EightBall/script.js
@@ -1,4 +1,5 @@
 import data from "./data.json";
+import axios from "axios";
 
 export default {
   data() {
@@ -17,12 +18,57 @@ export default {
       e.preventDefault();
       const index = [Math.floor(Math.random() * this.answers.length)];
       this.answer = this.answers[index];
+      this.sendToSlack(this.question, this.answer);
       this.toggle();
     },
     reset() {
       this.question = null;
       this.answer = null;
       this.toggle();
+    },
+    async sendToSlack(question, answer) {
+      try {
+        const url =
+          "https://hooks.slack.com/services/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";
+        const data = {
+          type: "mrkdwn",
+          text: "Hello World!",
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `*${new Date().toString()}*`
+              }
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `*Question*: ${question}`
+              }
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `*Answer*: ${answer}`
+              }
+            }
+          ]
+        };
+        await axios.post(url, JSON.stringify(data), {
+          withCredentials: false,
+          transformRequest: [
+            (data, headers) => {
+              delete headers.post["Content-Type"];
+              return data;
+            }
+          ]
+        });
+      } catch (error) {
+        return error;
+      }
     }
   },
   computed: {
