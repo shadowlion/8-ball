e8621855294493d17170e7d4a3c8d4e375ec36e2
Justin Chiou <chiou.kai@gmail.com>
2020-03-04 23:43:57 -0500

    refactor to use vuex, remove bootstrap-vue

---
+++ src/store/index.ts
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import { answers } from "../data";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    question: null,
+    answer: null
+  },
+  getters: {
+    question: state => state.question,
+    answer: state => state.answer,
+    shaken: state => state.answer !== null
+  },
+  mutations: {
+    SET_QUESTION(state, payload) {
+      state.question = payload;
+    },
+    SET_ANSWER(state, payload) {
+      state.answer = payload;
+    }
+  },
+  actions: {
+    setQuestion({ commit }, payload) {
+      commit("SET_QUESTION", payload);
+    },
+    setAnswer({ commit }) {
+      const index = Math.floor(Math.random() * answers.length);
+      commit("SET_ANSWER", answers[index]);
+    },
+    resetForm({ commit }) {
+      commit("SET_ANSWER", null);
+      commit("SET_QUESTION", null);
+    }
+  }
+});
